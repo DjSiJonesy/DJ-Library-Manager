3d4dd9bccdb29d30887b6a42b0e08f28aae1c9c7
DjSiJonesy <simon@smsounds.co.uk>
2026-07-10 22:52:51 +0100

    <Application     xmlns="https://github.com/avaloniaui"     xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"     x:Class="DJLibraryManager.UI.App">      <Application.Resources>          <ResourceDictionary>              <ResourceDictionary.MergedDictionaries>                  <ResourceInclude Source="avares://DJLibraryManager.UI/Themes/DJLMTheme.axaml"/>              </ResourceDictionary.MergedDictionaries>          </ResourceDictionary>      </Application.Resources>      <Application.Styles>          <FluentTheme/>      </Application.Styles>  </Application>
    - Created Avalonia UI project
    - Implemented MVVM structure
    - Added Startup Discovery screen
    - Created modular theme architecture
    - Added colour resource dictionary
    - Created style placeholders
    - Fixed Avalonia resource loading issues
    - Established UI project structure

--- UI/DJLibraryManager.UI/ViewModels/MainViewModel.cs
+++ UI/DJLibraryManager.UI/ViewModels/MainViewModel.cs
@@ -1,10 +1,25 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
 
-namespace DJLibraryManager.UI.ViewModels
+namespace DJLibraryManager.UI.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
 {
-    public partial class MainViewModel : ViewModelBase
+    public ObservableCollection<string> InstalledProviders { get; } = new()
     {
-        [ObservableProperty]
-        private string _greeting = "Welcome to Avalonia!";
-    }
+        "VirtualDJ",
+        "Rekordbox"
+    };
+
+    public ObservableCollection<string> MusicLocations { get; } = new()
+    {
+        @"C:\Users\Simon\Music",
+        @"D:\Music"
+    };
+
+    [ObservableProperty]
+    private bool isBusy;
+
+    [ObservableProperty]
+    private string statusText = "Ready";
 }
